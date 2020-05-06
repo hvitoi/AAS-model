bd50057f2ea3d627387582c0e2422008cf050eeb
Henrique Vitoi <hvitoi@gmail.com>
2020-05-06 08:21:45 -0300

    Public folder statement changed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
 const express = require('express') // Express exposes just a single function
 const cors = require('cors')
-const path = require('path')
-
-// Setup absolute path for index.html
-const indexFile= path.join(__dirname, '../public/index.html')
-
-
-
 
 // Initialize application
 const app = express()
@@ -22,11 +15,6 @@ app.use(express.json())
 
 // Setup routers
 app.use(require('./routers/asset-endpoint'))
-
-// Route for not matched URLs
-app.get('*', (req, res) => {
-    res.sendFile(indexFile)          
-})
 
 
 module.exports = app
